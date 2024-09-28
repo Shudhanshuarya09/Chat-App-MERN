40471a469fc1e05c7bed57a6ef06fac1f04cc402
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-09-28 17:41:56 +0530

    schema prepare

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -2,9 +2,7 @@ export const signup = (req, res) => {
  try {
   const {fullName, username, password, confirmPassword, gender } = req.body;
  }
- catch (error) {
-  
- }
+ catch (error) {}
 };
 
 export const login = (req, res) => {
