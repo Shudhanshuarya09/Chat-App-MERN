418b9bf130c8bee92d22589c468f59a02d84e240
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-09-30 18:16:55 +0530

    model file name changed

--- Backend/model/User.model.js
+++ Backend/model/User.model.js
@@ -26,6 +26,6 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model("User", userSchema);
+const user = mongoose.model("User", userSchema);
 
-export default User;
+export default user;
