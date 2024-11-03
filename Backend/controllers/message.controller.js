8da54e052af3a6c59e160b6519ff1afde2781b90
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-11-03 23:14:22 +0530

    changes in code

--- Backend/controllers/message.controller.js
+++ Backend/controllers/message.controller.js
@@ -32,11 +32,32 @@ export const sendMessage = async (req, res) => {
     if (newMessage) {
       conversation.messages.push(newMessage._id);
     }
-    await newMessage.save();
-    await conversation.save();
+    // await newMessage.save();
+    // await conversation.save();
+
+    await Promise.all([conversation.save(), newMessage.save()]);
+
+
     res.status(201).json(newMessage);
   } catch (error) {
     console.log("Error in sendMessage controller: ", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+export const getMessages = async  (req, res) => {
+  try {
+    const {id:userToChatId}= req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: {$all: [senderId, userToChatId]},
+    }).populate("messages");
+
+    res.status(200).json(conversation.messages);
+    
+  } catch (error) {
+    console.log("Error in sendMessage controller: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
