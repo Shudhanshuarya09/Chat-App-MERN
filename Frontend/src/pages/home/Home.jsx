3e6dab8acbc2c012248acbc6384bbf30286fda76
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-11-23 16:00:59 +0530

    message part fully complete

--- Frontend/src/pages/home/Home.jsx
+++ Frontend/src/pages/home/Home.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import Sidebar from '../../components/sidebar/Sidebar';
+import React from "react";
+import Sidebar from "../../components/sidebar/Sidebar";
+import MessageContainer from "../../components/messages/MessageContainer";
 
 const Home = () => {
   return (
-    <div>
-<Sidebar/>
-
+    <div className="flex rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 sm:h-[450px] md:h-[550px]">
+      <Sidebar />
+      <MessageContainer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
