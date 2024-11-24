88cd997e05dc3712f865a94ad31ca4f28da03bde
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-11-25 00:03:32 +0530

    Routes setup (frontend-backend connect)

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,17 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-// import Login from "./pages/login/Login";
-// import SignUp from "./pages/signup/signUp";
 import Home from "./pages/home/Home";
-function App() {
- 
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
 
+function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      {/* <SignUp/> */}
-     {/* HomePage */}
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </div>
   );
 }
