4add322d375675d6a0f8ab13e95c42ad90e4e59b
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-11-24 23:41:28 +0530

    frontend part complete

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,8 +2,8 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import "./App.css";
-import Login from "./pages/login/Login";
-import SignUp from "./pages/signup/signUp";
+// import Login from "./pages/login/Login";
+// import SignUp from "./pages/signup/signUp";
 import Home from "./pages/home/Home";
 function App() {
  
