647a8eec2a0ba19bef6dccba4ea028c25aee1863
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-11-24 23:22:46 +0530

    message container

--- Frontend/src/components/messages/MessageContainer.jsx
+++ Frontend/src/components/messages/MessageContainer.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import Messages from "./Messages";
 import MessageInput from "./MessageInput";
+import {TiMessages} from "react-icons/ti";
 
 const MessageContainer = () => {
+  const noChatSelected = true;
   return (
     <div className="md:min-w-[450px] flex flex-col">
-      <>
-        {/* Header */}
-        <div className="bg-slate-500 px-4  py-2 mb-2">
-          <span className="label-text">To:</span>
-          <span className="text-gray-900 font-bold "> Shudhanshu Arya</span>
-        </div>
+     {noChatSelected ? (<noChatSelected/>) : (
+       <>
+       {/* Header */}
+       <div className="bg-slate-500 px-4  py-2 mb-2">
+         <span className="label-text">To:</span>
+         <span className="text-gray-900 font-bold "> Shudhanshu Arya</span>
+       </div>
 
-        <Messages />
-        <MessageInput />
-      </>
+       <Messages />
+       <MessageInput />
+     </>
+     )}
     </div>
   );
 };
