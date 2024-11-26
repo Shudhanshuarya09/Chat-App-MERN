7c91970dff7c3092ff69b33fa27ca5b8a48fd65d
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-11-26 14:37:35 +0530

    input functionality

--- Frontend/src/pages/signup/signUp.jsx
+++ Frontend/src/pages/signup/signUp.jsx
@@ -1,7 +1,22 @@
 import GenderCheckbox from "./GenderCheckBox";
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
+
 
 const SignUp = () => {
+  const [inputs, setInputs] = useState({
+    fullName: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+    gender: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefalut();
+    console.log(inputs);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div
@@ -10,7 +25,7 @@ const SignUp = () => {
         <h1 className="text-3x1 font-semibold text-center Mltext-gray-3e0">
           Sign Up <span className="Mtext-blue-568"> ChatApp</span>
         </h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label className="label p-2">
               <span className="text-base label-text">Full Name</span>
@@ -18,7 +33,9 @@ const SignUp = () => {
             <input
               type="text"
               placeholder="Shudhanshu Arya"
-              className="w-full input input-bordered h-10"
+              className="w-full input input-bordered h-10" 
+              value={inputs.fullName}
+              onChange={(e) => setInputs({...inputs, fullName: e.target.value})}
             />
           </div>
 
@@ -28,6 +45,9 @@ const SignUp = () => {
               type="text"
               placeholder="arya@example.com"
               class="full input input-bordered"
+
+              value={inputs.username}
+              onChange={(e) => setInputs({...inputs, username: e.target.value})}
             />
           </div>
           <div>
@@ -36,6 +56,9 @@ const SignUp = () => {
               type="password"
               placeholder="Enter Password"
               class="full input input-bordered"
+
+              value={inputs.password}
+              onChange={(e) => setInputs({...inputs, password: e.target.value})}
             />
           </div>
 
@@ -45,12 +68,14 @@ const SignUp = () => {
               type="password"
               placeholder="Confirm Password"
               class="full input input-bordered"
+              value={inputs.confirmPassword}
+              onChange={(e) => setInputs({...inputs, confirmPassword: e.target.value})}
             />
           </div>
           <GenderCheckbox />
-          <Link to={'/login'}
-            className="tecxt-sm hover:underline hover:text-blue-600 mt-2 inline-block "
-            >
+          <Link
+            to={"/login"}
+            className="tecxt-sm hover:underline hover:text-blue-600 mt-2 inline-block ">
             Already have an account ?
           </Link>
 
@@ -65,3 +90,5 @@ const SignUp = () => {
   );
 };
 export default SignUp;
+
+//2:32
