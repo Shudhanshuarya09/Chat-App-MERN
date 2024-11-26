44fff7dc0d0384cb3a1cacfccf07eb9ffdb2235f
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-11-26 18:21:43 +0530

    signup all clear

--- Frontend/src/pages/signup/GenderCheckBox.jsx
+++ Frontend/src/pages/signup/GenderCheckBox.jsx
@@ -2,7 +2,7 @@ const GenderCheckbox = (onCheckboxChange, selectedGender) => {
   return (
     <div className="flex">
       <div className="form-control">
-        <label className={"label gap-2 cursor-pointer"}>
+        <label className={'label gap-2 cursor-pointer ${selectedGender === "male" ? "selected" : ""} '}>
           <span className="label-text">Male</span>
           <input type="checkbox" className="checkbox border-slate-900" 
           
@@ -12,7 +12,7 @@ const GenderCheckbox = (onCheckboxChange, selectedGender) => {
         </label>
       </div>
       <div className="form-control">
-        <label className={" label gap-2 cursor-pointer"}>
+        <label className={' label gap-2 cursor-pointer ${selectedGender === "male" ? "selected" : ""}'}>
           <span className="label-text">Female</span>
           <input type="checkbox" className="checkbox border-slate-980"
             checked={selectedGender === 'female'}
